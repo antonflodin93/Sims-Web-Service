5a07233a444e7658f2b8d77eb9bef2181d28e350
antonflodin93 <anfl1201@student.miun.se>
2017-10-15 00:56:33 +0200

    Added hashing with salt when the user creates a new account.

--- WS/src/main/java/org/test/WS/service/EmployeeService.java
+++ WS/src/main/java/org/test/WS/service/EmployeeService.java
@@ -1,5 +1,8 @@
 package org.test.WS.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,37 +26,37 @@ public class EmployeeService {
 
 	// Returns all the messages
 	public List<Employee> getEmployees() throws SQLException, ClassNotFoundException {
-			
+
 		result = new ArrayList<Employee>();
 		connection = DBConnection.setDBConnection();
 		String sql = "SELECT * from employees";
 		PreparedStatement pst = connection.prepareStatement(sql);
 		resultSet = pst.executeQuery();
-		
+
 		while (resultSet.next()) {
-			Employee m = new Employee(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4),
-					resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8));
+			Employee m = new Employee(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2),
+					resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),
+					resultSet.getString(7), resultSet.getString(8));
 			result.add(m);
 		}
 		pst.close();
-		
+
 		return result;
 	}
 
 	// Returns employee with id
 	public Employee getEmployee(int id) throws SQLException, ClassNotFoundException {
-		DBConnection connect = new DBConnection();
 		connection = DBConnection.setDBConnection();
 		String sql = "Select * from employees where employeeID = ?";
 		PreparedStatement pst = connection.prepareStatement(sql);
 		pst.setLong(1, id);
 		resultSet = pst.executeQuery();
 		Employee employee = new Employee();
-		if(resultSet.next() == true) {
-			employee = new Employee(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), 
-					resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8));
-		}
-		else {
+		if (resultSet.next() == true) {
+			employee = new Employee(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2),
+					resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),
+					resultSet.getString(7), resultSet.getString(8));
+		} else {
 			employee = null;
 		}
 		pst.close();
@@ -61,19 +64,57 @@ public class EmployeeService {
 	}
 
 	// Inserts employee into db
-	public Employee addEmployee(Employee employee) throws ClassNotFoundException, SQLException {
+	public Employee addEmployee(Employee employee)
+			throws ClassNotFoundException, SQLException {
+		
+		
+		// Generate salt
+		byte[] salt = new byte[16];
+		new SecureRandom().nextBytes(salt);
+
+		// Convert salt to string type
+		StringBuffer saltBuffer = new StringBuffer();
+		for (int i = 0; i < salt.length; i++) {
+			saltBuffer.append(Integer.toString((salt[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		String saltString = saltBuffer.toString();
+
+		// Add the salt to the password
+		String passwordandsalt = employee.getEmployeePassword() + saltString;
+		MessageDigest md;
+		
+		try {
+			// Hash it
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(passwordandsalt.getBytes());
+
+		} catch (NoSuchAlgorithmException e) {
+			return employee;
+		}
+
+		byte byteData[] = md.digest();
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		String hash = sb.toString();
+
+		
 		connection = DBConnection.setDBConnection();
-		String sql = "insert into employees (employeeId, employeeFirstName, employeeLastName, employeeUsername, "
-				+ "employeeEmail, employeePassword, employeePhonenumber, employeeCompany) values(?, ?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO employees (employeeId, employeeFirstName, employeeLastName, employeeUsername, "
+				+ "employeeEmail, employeePassword, employeePhonenumber, employeeCompany, employeeSalt) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		PreparedStatement pst = connection.prepareStatement(sql);
 		pst.setLong(1, employee.getEmployeeId());
 		pst.setString(2, employee.getEmployeeFirstName());
 		pst.setString(3, employee.getEmployeeLastName());
 		pst.setString(4, employee.getEmployeeUsername());
 		pst.setString(5, employee.getEmployeeEmail());
-		pst.setString(6, employee.getEmployeePassword());
+		pst.setString(6, hash);
 		pst.setString(7, employee.getEmployeePhonenumber());
 		pst.setString(8, employee.getEmployeeCompany());
+		pst.setString(9, saltString);
 		pst.executeUpdate();
 		pst.close();
 		return employee;
@@ -82,10 +123,9 @@ public class EmployeeService {
 	public Employee deleteEmployeeById(int id) throws SQLException, ClassNotFoundException {
 		// Get the employee that is going to be deleted
 		Employee e = getEmployee(id);
-		
-		DBConnection connect = new DBConnection();
+
 		connection = DBConnection.setDBConnection();
-		
+
 		String sql = "DELETE FROM employees WHERE employeeId = ?";
 		PreparedStatement pst = connection.prepareStatement(sql);
 		pst.setLong(1, id);
