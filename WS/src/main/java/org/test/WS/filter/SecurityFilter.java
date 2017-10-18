2753472870ec4a0c4258a3fa116f94854bb86428
antonflodin93 <anfl1201@student.miun.se>
2017-10-18 14:39:25 +0200

    Added login feature for employee or masterunit

--- WS/src/main/java/org/test/WS/filter/SecurityFilter.java
+++ WS/src/main/java/org/test/WS/filter/SecurityFilter.java
@@ -27,33 +27,55 @@ public class SecurityFilter implements ContainerRequestFilter {
 	private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
 	private static final String AUTHORIZATION_HEADER_PREFIX = "Basic ";
 	private static final String SECURED_URL_PREFIX = "secured";
+	private static final String SECURED_MASTER_LOGIN_URL_PREFIX = "secured/login/master";
+	private static final String SECURED_EMPLOYEE_LOGIN_URL_PREFIX = "secured/login/employee";
+
+	private enum UserType {
+		MASTER, EMPLOYEE;
+	}
+
+	UserType userType;
+
 	private String username, password;
 	private static final Logger logger = Logger.getLogger("SecurityFilter");
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		// Check if the url consists of the secured url prefix
-		if (requestContext.getUriInfo().getPath().contains(SECURED_URL_PREFIX)) {
+		if (requestContext.getUriInfo().getPath().contains(SECURED_MASTER_LOGIN_URL_PREFIX)) {
 
-			//
+			// Check if the user is employee or master
+			if (requestContext.getUriInfo().getPath().contains(SECURED_MASTER_LOGIN_URL_PREFIX)) {
+				userType = UserType.MASTER;
+			} else if (requestContext.getUriInfo().getPath().contains(SECURED_EMPLOYEE_LOGIN_URL_PREFIX)) {
+				userType = UserType.EMPLOYEE;
+			}
+
+			// Get list of the authorization headers
 			List<String> authorizationHeaders = requestContext.getHeaders().get(AUTHORIZATION_HEADER_KEY);
 
-			//
+			// Check if there is any headers
 			if (authorizationHeaders != null && authorizationHeaders.size() > 0) {
-				//
+				// Get the authorization token
 				String authorizationToken = authorizationHeaders.get(0);
 				authorizationToken = authorizationToken.replaceFirst(AUTHORIZATION_HEADER_PREFIX, "");
 
-				//
+				// Decode the authorization token with base64 coding
 				String decodedString = Base64.decodeAsString(authorizationToken);
-				// String decodedString = authorizationToken;
 
-				//
+				// Get tokenizer of the decoded strings
 				StringTokenizer tokenizer = new StringTokenizer(decodedString, ":");
 
-				//
+				// Get the username and password
 				username = tokenizer.nextToken();
 				password = tokenizer.nextToken();
+
+				FileHandler fh;
+				// This block configure the logger with handler and formatter
+				fh = new FileHandler("C:/Users/Student/Desktop/DEV/Logs/MyLogFile.log");
+				SimpleFormatter formatter = new SimpleFormatter();
+				fh.setFormatter(formatter);
+				logger.addHandler(fh);
 
 				// Check if user exists
 				try {
@@ -61,8 +83,7 @@ public class SecurityFilter implements ContainerRequestFilter {
 						return;
 					}
 				} catch (ClassNotFoundException | SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					logger.info("ERROR: " + e.getMessage());
 				}
 
 			}
@@ -77,28 +98,31 @@ public class SecurityFilter implements ContainerRequestFilter {
 
 	}
 
-	private boolean correctCredentials() throws SQLException, ClassNotFoundException, SecurityException, IOException {
-		FileHandler fh;
-		// This block configure the logger with handler and formatter
-		fh = new FileHandler("C:/Users/Student/Desktop/DEV/MyLogFile.log");
-		logger.addHandler(fh);
-		SimpleFormatter formatter = new SimpleFormatter();
-		fh.setFormatter(formatter);
-
-		// the following statement is used to log any messages
-		logger.info("In function");
-
+	private boolean correctCredentials() throws ClassNotFoundException, SecurityException, IOException, SQLException {
 		boolean correctcredentials = false;
-		Connection connection;
+		Connection connection = null;
 		connection = DBConnection.setDBConnection();
-		// Get the hashed password and the salt for the username or email
-		String sql = "SELECT employeePassword, employeeSalt FROM employees WHERE employeeUsername = (?) OR employeeEmail = (?)";
-		PreparedStatement pst = connection.prepareStatement(sql);
+
+		String sql = null;
+		// Get the hashed password and the salt for the username or email for the
+		// specific role
+		if (userType == UserType.MASTER) {
+			sql = "SELECT employeePassword, employeeSalt FROM employees WHERE (employeeUsername = (?) OR employeeEmail = (?)) AND employeeRole = 'MASTER'";
+			logger.info("MASTER \n sql: " + sql);
+
+		} else if (userType == UserType.EMPLOYEE) {
+			logger.info("EMPLOYEE");
+			sql = "SELECT employeePassword, employeeSalt FROM employees WHERE employeeUsername = (?) OR employeeEmail = (?)";
+		}
+
+		PreparedStatement pst = null;
+		ResultSet resultSet = null;
+		logger.info("AFTER IF");
+
+		pst = connection.prepareStatement(sql);
 		pst.setString(1, username);
 		pst.setString(2, username);
-		ResultSet resultSet = pst.executeQuery();
-		logger.info("sql: " + sql);
-		logger.info(password);
+		resultSet = pst.executeQuery();
 
 		// Check if the result is empty
 		if (resultSet != null) {
@@ -126,40 +150,18 @@ public class SecurityFilter implements ContainerRequestFilter {
 				}
 
 				String hashgenerated = sb.toString();
-				logger.info(hashgenerated + "(generated)");
-				logger.info(hash + "(generated)");
+				// logger.info(hashgenerated + "(generated)");
+				// logger.info(hash + "(generated)");
 
 				if (hashgenerated.equals(hash)) {
 					correctcredentials = true;
 				}
 
-			} else {
-				logger.info("null response");
 			}
-		} else {
-			logger.info("null response");
 		}
-
 		pst.close();
 
 		return correctcredentials;
-
-		/*
-		 * boolean correctcredentials = false; Connection connection; connection =
-		 * DBConnection.setDBConnection(); String sql =
-		 * "SELECT * FROM employees WHERE (employeeUsername = (?) OR employeeEmail = (?)) AND employeePassword = (?)"
-		 * ; PreparedStatement pst = connection.prepareStatement(sql); pst.setString(1,
-		 * username); pst.setString(2, username); pst.setString(3, password); ResultSet
-		 * resultSet = pst.executeQuery();
-		 * 
-		 * if (resultSet != null) { resultSet.beforeFirst(); resultSet.last();
-		 * if(resultSet.getRow() > 0) { correctcredentials = true; } }
-		 * 
-		 * 
-		 * pst.close();
-		 * 
-		 * return correctcredentials;
-		 */
 	}
 
 }
--- WS/src/main/java/org/test/WS/resources/SecureResource.java
+++ WS/src/main/java/org/test/WS/resources/SecureResource.java
@@ -3,7 +3,6 @@ package org.test.WS.resources;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,12 +11,21 @@ import javax.ws.rs.core.MediaType;
 @Path("secured")
 public class SecureResource {
 
-	// Returns all employees
+	// Login as master
 	@GET
-	@Path("login")
+	@Path("login/master")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response testLogin() throws ClassNotFoundException, SQLException {
-		Response response = Response.status(Response.Status.ACCEPTED).entity("User is granted access").build();
+	public Response loginMaster() throws ClassNotFoundException, SQLException {
+		Response response = Response.status(Response.Status.ACCEPTED).entity("Masterunit is granted access").build();
+		return response;
+	}
+
+	// Login as employee
+	@GET
+	@Path("login/employee")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response loginEmployee() throws ClassNotFoundException, SQLException {
+		Response response = Response.status(Response.Status.ACCEPTED).entity("Employee is granted access").build();
 		return response;
 	}
 
