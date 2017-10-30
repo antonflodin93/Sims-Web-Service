6b7081832703194682323592cbc4aa57e26e7d82
antonflodin93 <anfl1201@student.miun.se>
2017-10-30 07:57:59 +0100

    Added get all companies method.

--- WS/src/main/java/org/test/WS/resources/CompanyResource.java
+++ WS/src/main/java/org/test/WS/resources/CompanyResource.java
@@ -2,15 +2,12 @@ package org.test.WS.resources;
 
 import java.sql.SQLException;
 import java.util.List;
-
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.test.WS.model.Message;
-import org.test.WS.resources.MessageResource.MessageType;
+import org.test.WS.model.Company;
 import org.test.WS.service.CompanyService;
 
 @Path("companies")
@@ -20,8 +17,7 @@ public class CompanyResource {
 	// Return all the companies
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAllCompanies() throws ClassNotFoundException, SQLException {
+	public List<Company> getAllCompanies() throws ClassNotFoundException, SQLException {
 		return companyService.getAllCompanies();
 	}
-
 }
--- WS/src/main/java/org/test/WS/service/CompanyService.java
+++ WS/src/main/java/org/test/WS/service/CompanyService.java
@@ -1,23 +1,46 @@
 package org.test.WS.service;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.test.WS.database.DBConnection;
 import org.test.WS.model.Company;
-import javax.ws.rs.core.Response;
+
 
 public class CompanyService {
 	DBConnection dBconnection;
 	Connection connection;
 	ResultSet resultSet;
-	List<Company> result;
-	
-	
-	public Response getAllCompanies() {
-		// TODO Auto-generated method stub
-		return null;
+	List<Company> companies;
+
+	// Returns all companies
+	public List<Company> getAllCompanies() throws SQLException {
+		try {
+			companies = new ArrayList<Company>();
+			connection = DBConnection.setDBConnection();
+			String sql = "SELECT * from companies";
+			PreparedStatement pst = connection.prepareStatement(sql);
+			resultSet = pst.executeQuery();
+
+			while (resultSet.next()) {
+
+				Company company = new Company(resultSet.getString("companyName"));
+				companies.add(company);
+			}
+
+			pst.close();
+			connection.close();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			connection.close();
+
+		}
+
+		return companies;
 	}
 
 }
