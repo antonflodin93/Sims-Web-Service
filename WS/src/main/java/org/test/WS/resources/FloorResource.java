44e2e01845370e96de547b0f7c43e13a842e146f
antonflodin93 <anfl1201@student.miun.se>
2017-11-28 08:51:55 +0100

    Added get method to get a certain floor

--- WS/src/main/java/org/test/WS/resources/FloorResource.java
+++ WS/src/main/java/org/test/WS/resources/FloorResource.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import org.test.WS.model.Floor;
 import org.test.WS.service.FloorService;
 
@@ -16,24 +18,38 @@ import org.test.WS.service.FloorService;
 public class FloorResource {
 	private FloorService floorService = new FloorService();
 	private List<Floor> floors = null;
+	private Floor floor;
 
-	// Return all the buildings
+	// Return all the floors
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllFloors() {
 		try {
 			floors = floorService.getAllFloors();
-		} catch(ClassNotFoundException e) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-			.entity(e.getMessage()).build();
-			
+		} catch (ClassNotFoundException e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+
 		} catch (SQLException e) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-			.entity(e.getMessage()).build();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
 		}
-		
-	    return Response.ok(new GenericEntity<List<Floor>>(floors) {}).build();
+
+		return Response.ok(new GenericEntity<List<Floor>>(floors) {
+		}).build();
+	}
+
+	// Get floor by Id
+	@GET
+	@Path("/{floorId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getEmployeeMessage(@PathParam("floorId") int floorId)
+			throws ClassNotFoundException, SQLException {
+		try {
+			floor = floorService.getFloorById(floorId);
+
+		} catch (SQLException | ClassNotFoundException e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
+		return Response.status(Response.Status.OK).entity(floor).build();
 	}
 
 }
-
--- WS/src/main/java/org/test/WS/service/FloorService.java
+++ WS/src/main/java/org/test/WS/service/FloorService.java
@@ -17,49 +17,97 @@ public class FloorService {
 	ResultSet resultSet;
 	List<Floor> floors;
 
-
 	public List<Floor> getAllFloors() throws ClassNotFoundException, SQLException {
 		floors = new ArrayList<Floor>();
 		connection = DBConnection.setDBConnection();
 		String sql = "SELECT * from floors";
 		PreparedStatement pst = connection.prepareStatement(sql);
 		resultSet = pst.executeQuery();
-		
+
 		// For each row, get the corresponding objects
 		while (resultSet.next()) {
-			
+
 			// Get the id of the floor
 			int floorId = Integer.parseInt(resultSet.getString("floorId"));
-			
+
 			ArrayList<FactoryObject> objects = new ArrayList<FactoryObject>();
 			Connection innerConnection = DBConnection.setDBConnection();
 			String sqlObjects = "SELECT * from factoryobjects where objectFloorId = ?";
 			PreparedStatement pstObjects = connection.prepareStatement(sqlObjects);
 			pstObjects.setLong(1, floorId);
 			ResultSet resultSetObjects = pstObjects.executeQuery();
-			
+
 			// Create floor objects
 			while (resultSetObjects.next()) {
-				
-				FactoryObject object = new FactoryObject(Integer.parseInt(resultSetObjects.getString("objectId")), resultSetObjects.getString("objectName"), Integer.parseInt(resultSetObjects.getString("objectFloorId")),
-						Integer.parseInt(resultSetObjects.getString("areaXStart")), Integer.parseInt(resultSetObjects.getString("areaXEnd")), Integer.parseInt(resultSetObjects.getString("areaYStart")), Integer.parseInt(resultSetObjects.getString("areaYEnd")));
+
+				FactoryObject object = new FactoryObject(Integer.parseInt(resultSetObjects.getString("objectId")),
+						resultSetObjects.getString("objectName"),
+						Integer.parseInt(resultSetObjects.getString("objectFloorId")),
+						Integer.parseInt(resultSetObjects.getString("areaXStart")),
+						Integer.parseInt(resultSetObjects.getString("areaXEnd")),
+						Integer.parseInt(resultSetObjects.getString("areaYStart")),
+						Integer.parseInt(resultSetObjects.getString("areaYEnd")));
 				objects.add(object);
 			}
-											
-		
-			Floor floor = new Floor(Integer.parseInt(resultSet.getString("floorId")), 
-						resultSet.getString("floorLevel"), resultSet.getString("floorPlanFilePath"), Integer.parseInt(resultSet.getString("floorBuildingId")), objects);
+
+			Floor floor = new Floor(Integer.parseInt(resultSet.getString("floorId")), resultSet.getString("floorLevel"),
+					resultSet.getString("floorPlanFilePath"), Integer.parseInt(resultSet.getString("floorBuildingId")),
+					objects);
 			floors.add(floor);
-			
+
 			pstObjects.close();
 			innerConnection.close();
-		
+
 		}
 
 		pst.close();
 		connection.close();
 
+		return floors;
+	}
 
-	return floors;
+	public Floor getFloorById(int floorId) throws ClassNotFoundException, SQLException {
+		Floor floor = null;
+		connection = DBConnection.setDBConnection();
+		String sql = "SELECT * from floors WHERE floorId = ?";
+		PreparedStatement pst = connection.prepareStatement(sql);
+		pst.setLong(1, floorId);
+		resultSet = pst.executeQuery();
+
+		if (resultSet.next()) {
+
+			ArrayList<FactoryObject> objects = new ArrayList<FactoryObject>();
+			Connection innerConnection = DBConnection.setDBConnection();
+			String sqlObjects = "SELECT * from factoryobjects where objectFloorId = ?";
+			PreparedStatement pstObjects = connection.prepareStatement(sqlObjects);
+			pstObjects.setLong(1, floorId);
+			ResultSet resultSetObjects = pstObjects.executeQuery();
+
+			// Create floor objects
+			while (resultSetObjects.next()) {
+
+				FactoryObject object = new FactoryObject(Integer.parseInt(resultSetObjects.getString("objectId")),
+						resultSetObjects.getString("objectName"),
+						Integer.parseInt(resultSetObjects.getString("objectFloorId")),
+						Integer.parseInt(resultSetObjects.getString("areaXStart")),
+						Integer.parseInt(resultSetObjects.getString("areaXEnd")),
+						Integer.parseInt(resultSetObjects.getString("areaYStart")),
+						Integer.parseInt(resultSetObjects.getString("areaYEnd")));
+				objects.add(object);
+			}
+
+			floor = new Floor(Integer.parseInt(resultSet.getString("floorId")), resultSet.getString("floorLevel"),
+					resultSet.getString("floorPlanFilePath"), Integer.parseInt(resultSet.getString("floorBuildingId")),
+					objects);
+
+			pstObjects.close();
+			innerConnection.close();
+
+		}
+
+		pst.close();
+		connection.close();
+
+		return floor;
 	}
 }
