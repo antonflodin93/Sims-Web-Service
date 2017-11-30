e9eb986fedd76aa1cba28af53997e1078dce111f
antonflodin93 <anfl1201@student.miun.se>
2017-11-30 23:11:56 +0100

    Login now return employeeId that will be used in the application

--- WS/src/main/java/org/test/WS/filter/SecurityFilter.java
+++ WS/src/main/java/org/test/WS/filter/SecurityFilter.java
@@ -26,6 +26,7 @@ public class SecurityFilter implements ContainerRequestFilter {
 	private static final String SECURED_URL_PREFIX = "secured";
 	private static final String SECURED_MASTER_LOGIN_URL_PREFIX = "secured/login/master";
 	private static final String SECURED_EMPLOYEE_LOGIN_URL_PREFIX = "secured/login/employee";
+	private String employeeID;
 
 	private enum UserType {
 		MASTER, EMPLOYEE;
@@ -83,6 +84,9 @@ public class SecurityFilter implements ContainerRequestFilter {
 				// Check if user exists
 				try {
 					if (correctCredentials()) {
+						if(userType == UserType.EMPLOYEE) {
+							requestContext.getHeaders().add("employeeID", employeeID);
+						}
 						return;
 					}
 				} catch (ClassNotFoundException | SQLException e) {
@@ -115,7 +119,7 @@ public class SecurityFilter implements ContainerRequestFilter {
 
 		} else if (userType == UserType.EMPLOYEE) {
 			//logger.info("EMPLOYEE SQL");
-			sql = "SELECT employeePassword, employeeSalt FROM employees WHERE (employeeUsername = (?) OR employeeEmail = (?)) AND employeeRole = 'EMPLOYEE'";
+			sql = "SELECT employeePassword, employeeSalt, employeeID FROM employees WHERE (employeeUsername = (?) OR employeeEmail = (?)) AND employeeRole = 'EMPLOYEE'";
 		}
 
 		PreparedStatement pst = null;
@@ -129,6 +133,9 @@ public class SecurityFilter implements ContainerRequestFilter {
 		// Check if the result is empty
 		if (resultSet != null) {
 			if (resultSet.next() == true) {
+				if(userType == UserType.EMPLOYEE) {
+					employeeID = String.valueOf(resultSet.getLong("employeeID"));
+				}
 				String hash = resultSet.getString("employeePassword");
 				String salt = resultSet.getString("employeeSalt");
 				String passwordAndSalt = password + salt;
--- WS/src/main/java/org/test/WS/resources/SecureResource.java
+++ WS/src/main/java/org/test/WS/resources/SecureResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -16,7 +17,7 @@ public class SecureResource {
 	@Path("login/master")
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response loginMaster() throws ClassNotFoundException, SQLException {
-		Response response = Response.status(Response.Status.ACCEPTED).entity("Masterunit is granted access").build();
+		Response response = Response.status(Response.Status.ACCEPTED).build();
 		return response;
 	}
 
@@ -24,8 +25,9 @@ public class SecureResource {
 	@GET
 	@Path("login/employee")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response loginEmployee() throws ClassNotFoundException, SQLException {
-		Response response = Response.status(Response.Status.ACCEPTED).entity("Employee is granted access").build();
+	public Response loginEmployee(@HeaderParam("employeeID") String employeeID) throws ClassNotFoundException, SQLException {
+		
+		Response response = Response.status(Response.Status.ACCEPTED).entity(employeeID).build();
 		return response;
 	}
 	
